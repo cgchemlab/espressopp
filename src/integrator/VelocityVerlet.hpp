8d185d9d127b18bdefcbeacb1f11571886d87e07
Jakub Krajniak <jkrajniak@gmail.com>
2014-10-30 11:16:55 +0100

    update the timers, so that it is possible to measure for each of the interaction and also time required for processing signals in the Integrator.

--- src/integrator/VelocityVerlet.hpp
+++ src/integrator/VelocityVerlet.hpp
@@ -3,21 +3,21 @@
       Max Planck Institute for Polymer Research
   Copyright (C) 2008,2009,2010,2011
       Max-Planck-Institute for Polymer Research & Fraunhofer SCAI
-  
+
   This file is part of ESPResSo++.
-  
+
   ESPResSo++ is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   ESPResSo++ is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 // ESPP_CLASS
@@ -50,29 +50,30 @@ namespace espresso {
 
         void setIsokinetic(shared_ptr<class Isokinetic> isokinetic);
         shared_ptr<class Isokinetic> getIsokinetic() { return isokinetic; }
-        
+
         // set & get barostat (Berendsen)
         void setBerendsenBarostat(shared_ptr<class BerendsenBarostat> berendsenBarostat);
         shared_ptr<class BerendsenBarostat> getBerendsenBarostat() { return berendsenBarostat; }
-        
+
         // set & get thermostat (Berendsen)
         void setBerendsenThermostat(shared_ptr<class BerendsenThermostat> berendsenThermostat);
         shared_ptr<class BerendsenThermostat> getBerendsenThermostat() { return berendsenThermostat; }
-        
+
         // set & get barostat (Langevin-Hoover)
         void setLangevinBarostat(shared_ptr<class LangevinBarostat> langevinBarostat);
         shared_ptr<class LangevinBarostat> getLangevinBarostat() { return langevinBarostat; }
-        
+
         // set & get FixPositions
         void setFixPositions(shared_ptr <class FixPositions> _fixPositions);
         shared_ptr<class FixPositions> getFixPositions () {return fixPositions; }
         */
 
         void run(int nsteps);
-        
-        /** Load timings in array to export to Python as a tuple. */
-        void loadTimers(real t[10]);
 
+        /** Load timings in array to export to Python as a tuple. */
+        void loadTimers(std::vector<real> &return_vector);
+
+        /** Clean up all timers.*/
         void resetTimers();
 
 
@@ -117,9 +118,9 @@ namespace espresso {
         shared_ptr< class StochasticVelocityRescaling > stochasticVelocityRescaling;  //!< Stochastic velocity rescaling thermostat if available
         shared_ptr< class BerendsenBarostat > berendsenBarostat;  //!< Berendsen barostat if available
         shared_ptr< class BerendsenThermostat> berendsenThermostat;  //!< Berendsen thermostat if available
-        
+
         shared_ptr< class LangevinBarostat > langevinBarostat;  //!< Langevin-Hoover barostat if available
-        
+
         shared_ptr< class FixPositions > fixPositions; // fix positions of a group of particles
         */
 
@@ -152,12 +153,23 @@ namespace espresso {
         real timeRun;
         real timeLost;
         real timeForce;
-        real timeForceComp[100];
+        std::vector<real> timeForceComp;
         real timeComm1;
         real timeComm2;
         real timeInt1;
         real timeInt2;
         real timeResort;
+
+        // Signal timers
+        real timeRunInitS;
+        real timeRecalc1S;
+        real timeRecalc2S;
+        real timeBefIntPS;
+        real timeAftIntPS;
+        real timeAftInitFS;
+        real timeAftCalcFS;
+        real timeBefIntVS;
+        real timeAftIntVS;
 
         static LOG4ESPP_DECL_LOGGER(theLogger);
     };
