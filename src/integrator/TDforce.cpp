1d1e339fac81ac98aca5b522bb63b2c113089f58
Jakub Krajniak <jkrajniak@gmail.com>
2014-12-19 13:03:22 +0100

    Fixed TD force computation

--- src/integrator/TDforce.cpp
+++ src/integrator/TDforce.cpp
@@ -35,126 +35,114 @@
 
 
 namespace espresso {
-  using namespace iterator;
+using namespace iterator;  // NOLINT
+namespace integrator {
 
-  namespace integrator {
+LOG4ESPP_LOGGER(TDforce::theLogger, "TDforce");
 
-    LOG4ESPP_LOGGER(TDforce::theLogger, "TDforce");
+TDforce::TDforce(shared_ptr<System> system) : Extension(system) {
+  type = Extension::FreeEnergyCompensation;
 
-    TDforce::TDforce(shared_ptr<System> system)
-    :Extension(system) {
+  center = (0.0, 0.0, 0.0);
+  LOG4ESPP_INFO(theLogger, "TDforce constructed");
+}
 
-        type = Extension::FreeEnergyCompensation;
+TDforce::~TDforce() {}
 
-        center = (0.0,0.0,0.0);
-        LOG4ESPP_INFO(theLogger, "TDforce constructed");
+void TDforce::connect() {
+  _applyForce = integrator->aftCalcF.connect(
+  boost::bind(&TDforce::applyForce, this));
+}
+
+void TDforce::disconnect() {
+  _applyForce.disconnect();
+}
+
+
+void TDforce::addForce(int itype, const char* _filename, int type, int force_type) {
+  boost::mpi::communicator world;
+  filename = _filename;
+  Table table;
+
+  if (force_type != 0 && force_type != 1)
+    throw std::runtime_error("Wrong TD force type. Set 0 for x-direction and 1 for radius");
+  force_type_ = force_type;
+
+  switch (itype) {
+    case 1:  // InterpolationLinear
+      table = make_shared <interaction::InterpolationLinear> ();
+      table->read(world, _filename);
+      break;
+    case 2:  // InterpolationAkima
+      table = make_shared <interaction::InterpolationAkima> ();
+      table->read(world, _filename);
+      break;
+    case 3:  // InterpolationCubic
+      table = make_shared <interaction::InterpolationCubic> ();
+      table->read(world, _filename);
+      break;
+    default:
+      throw std::runtime_error("Wrong type of interpolation.");
+  }
+  forces.insert(std::make_pair(type, table));
+}
+
+
+void TDforce::applyForce() {
+  LOG4ESPP_DEBUG(theLogger, "apply TD force");
+
+  System& system = getSystemRef();
+
+  // iterate over CG particles
+  CellList cells = system.storage->getRealCells();
+  for (CellListIterator cit(cells); !cit.isDone(); ++cit) {
+    Table table = forces.find(cit->getType())->second;
+    if (table) {
+      // calculate distance from reference point
+      Real3D dl;
+      system.bc->getMinimumImageVector(dl, cit->getPos(), center);
+
+      if (force_type_ == 1) {
+        real dist = dl.abs();
+
+        // read fforce from table
+        real fforce = table->getForce(dist);
+        fforce /= dist;
+
+        // substract td force
+        cit->force() += (dl * fforce);
+
+      } else if (force_type_ == 0) {
+        // use this if you need 1-dir force only!
+        real force = table->getForce(fabs(dl[0])) * (dl[0] / fabs(dl[0]));
+        cit->force()[0] += force;
+      }
     }
+  }
+}
 
-    TDforce::~TDforce() {}
+void TDforce::setCenter(real x, real y, real z) {
+  center = Real3D(x, y, z);
+}
 
+/****************************************************
+** REGISTRATION WITH PYTHON
+****************************************************/
+void TDforce::registerPython() {
+  using namespace espresso::python;  //NOLINT
 
+  void (TDforce::*pySetCenter)(real x, real y, real z) = &TDforce::setCenter;
 
-    void TDforce::connect(){
-        _applyForce = integrator->aftCalcF.connect(
-            boost::bind(&TDforce::applyForce, this));
-    }
+  void (TDforce::*pyAddForce)(int itype, const char* filename, int type, int force_type)
+      = &TDforce::addForce;
 
-    void TDforce::disconnect(){
-        _applyForce.disconnect();
-    }
-
-
-    void TDforce::addForce(int itype, const char* _filename, int type, int force_type) {
-        boost::mpi::communicator world;
-        filename = _filename;
-        Table table;
-
-        if (force_type != 0 && force_type != 1)
-          throw std::runtime_error("Wrong TD force type. Set 0 for x-direction and 1 for radius");
-        force_type_ = force_type;
-
-        if (itype == 1) { // create a new InterpolationLinear
-            table = make_shared <interaction::InterpolationLinear> ();
-            table->read(world, _filename);
-        }
-
-        else if (itype == 2) { // create a new InterpolationAkima
-            table = make_shared <interaction::InterpolationAkima> ();
-            table->read(world, _filename);
-        }
-
-        else if (itype == 3) { // create a new InterpolationCubic
-            table = make_shared <interaction::InterpolationCubic> ();
-            table->read(world, _filename);
-        }
-
-        forces.insert(std::make_pair(type,table));
-    }
-
-
-    void TDforce::applyForce() {
-          LOG4ESPP_DEBUG(theLogger, "apply TD force");
-
-          System& system = getSystemRef();
-
-          // iterate over CG particles
-          CellList cells = system.storage->getRealCells();
-          for(CellListIterator cit(cells); !cit.isDone(); ++cit) {
-
-              Table table = forces.find(cit->getType())->second;
-
-              if (table) {
-                  // calculate distance from reference point
-                  if (force_type_ == 1){
-                    Real3D dist3D = cit->getPos() - center;
-                    real dist = sqrt(dist3D.sqr());
-
-                    // read fforce from table
-                    real fforce = table->getForce(dist);
-                    fforce /= dist;
-
-                    // substract td force
-                    cit->force() -= (dist3D * fforce);
-
-                  } else if (force_type_ == 0){
-                    // use this if you need 1-dir force only!
-                    Real3D dl;
-                    system.bc->getMinimumImageVector(dl, cit->getPos(), center);
-                    real force = table->getForce(dl[0]);
-                    cit->force()[0] -= force;
-                  }
-              }
-          }
-    }
-
-
-    void TDforce::setCenter(real x, real y, real z){
-            center = Real3D(x, y, z);
-    }
-
-    /****************************************************
-    ** REGISTRATION WITH PYTHON
-    ****************************************************/
-
-    void TDforce::registerPython() {
-
-      using namespace espresso::python;
-
-      void (TDforce::*pySetCenter)(real x, real y, real z)
-                        = &TDforce::setCenter;
-
-      void (TDforce::*pyAddForce)(int itype, const char* filename, int type, int force_type)
-                        = &TDforce::addForce;
-
-      class_<TDforce, shared_ptr<TDforce>, bases<Extension> >
-        ("integrator_TDforce", init< shared_ptr<System> >())
+  class_<TDforce, shared_ptr<TDforce>, bases<Extension> >
+      ("integrator_TDforce", init< shared_ptr<System> >())
         .add_property("filename", &TDforce::getFilename)
         .def("connect", &TDforce::connect)
         .def("disconnect", &TDforce::disconnect)
         .def("setCenter", pySetCenter)
-        .def("addForce", pyAddForce)
-        ;
-    }
-
-  }
+        .def("addForce", pyAddForce);
 }
+}  // namespace integrator
+}  // namespace espresso
