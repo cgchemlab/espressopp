76ce51e889c036343f18fcbd38c373717ce6d920
Jakub Krajniak <jkrajniak@gmail.com>
2015-12-03 16:36:55 +0100

    Update comments for methods and data structures.

--- src/integrator/TopologyManager.hpp
+++ src/integrator/TopologyManager.hpp
@@ -36,51 +36,109 @@
 namespace espressopp {
 namespace integrator {
 
-class TopologyManager : public Extension {
+class TopologyManager: public Extension {
  public:
   TopologyManager(shared_ptr<System> system);
   ~TopologyManager();
 
+  /**
+   * Pair list registration. The following types
+   * are used to update those list based on the new
+   * bond that appear in the system.
+   */
   /**
    * Observe fixed pair list against changes.
    * @param fpl FixedPairList
    */
   void observeTuple(shared_ptr<FixedPairList> fpl,
                     longint type1, longint type2);
+  /**
+   * Register triple pair list with desire atom types.
+   * @param ftl FixedTripleList
+   * @param type1 The longint type_1
+   * @param type2 The longint type_2
+   * @param type3 The lontint type_3
+   */
   void observeTriple(shared_ptr<FixedTripleList> ftl,
                      longint type1, longint type2, longint type3);
+  /**
+   * Register quadruple list with desire atom types.
+   * @param fql FixedQuadrupleList
+   * @param type1 The longint type_1
+   * @param type2 The longint type_2
+   * @param type3 The longint type_3
+   * @param type4 The longint type_4
+   */
   void observeQuadruple(shared_ptr<FixedQuadrupleList> fql,
                         longint type1, longint type2, longint type3, longint type4);
 
   /**
-   * Initial topology.
+   * Initialized topology by looking for bonds in registered PairLists and
+   * build adjacent list. Then this list is distributed among cpus so
+   * everyone has the same adjacent list.
    */
   void InitializeTopology();
+  /**
+   * Print adjacent list.
+   */
   void PrintTopology();
 
-  /** Rebuild map res_id -> particle_id */
+  /**
+   * Rebuild the map of particle_id -> res_id and sync among cpus.
+   */
   void Rebuild();
 
   static void registerPython();
 
   /// Defines map that stores res_id -> particle_id
   typedef std::set<longint> PSet;
-  typedef std::map<longint, PSet*> ResParticleIds;
+  typedef std::map<longint, PSet *> ResParticleIds;
 
  private:
   typedef std::pair<longint, std::pair<longint, longint> > Triplets;
   typedef std::pair<longint, std::pair<longint, std::pair<longint, longint> > > Quadruplets;
   typedef std::vector<std::pair<longint, longint> > EdgesVector;
 
-  /** Handle local tuple update. */
+  /**
+   * Handle signal from FixedPairList that new bond was created.
+   */
   void onTupleAdded(longint pid1, longint pid2);
+  /**
+   * Process new bond.
+   */
   void newBond(longint pid1, longint pid2);
+  /**
+   * Process new edge in topology.
+   */
   void newEdge(longint pid1, longint pid2);
+  /**
+   * Update registered FixedTripleList with new entries.
+   */
   void updateAngles(std::set<Triplets> &triplets);
+  /**
+   * Update registered FixedQuadrupleLists with new entries.
+   */
   void updateDihedrals(std::set<Quadruplets> &quadruplets);
-  void generateAnglesDihedrals(longint pid1, longint pid2, std::set<Quadruplets> &quadruplets, std::set<Triplets> &triplets);
+  /**
+   * Based on topology, generate missing angles and dihedrals around newly
+   * created bond.
+   */
+  void generateAnglesDihedrals(longint pid1,
+                               longint pid2,
+                               std::set<Quadruplets> &quadruplets,
+                               std::set<Triplets> &triplets);
+  /**
+   * Exchange new topology and res_id data among cpus.
+   */
   void exchangeData();
+  /**
+   * Merge two sets that holds mapping particle_id -> res_id
+   * and update res_id of corresponding particles.
+   */
   void mergeResIdSets(longint res_id_a, longint res_id_b);
+  /**
+   * Connecting/Disconnecting to signals.
+   */
   void connect();
   void disconnect();
 
@@ -107,7 +165,7 @@ class TopologyManager : public Extension {
               boost::unordered_map<
                   longint,
                   shared_ptr<FixedQuadrupleList> > > > > QuadrupleMap;
-  typedef std::map<longint, std::set<int>* > GraphMap;
+  typedef std::map<longint, std::set<int> *> GraphMap;
   bool update_angles_dihedrals;
 
   std::vector<shared_ptr<FixedPairList> > tupleMap_;
@@ -116,6 +174,7 @@ class TopologyManager : public Extension {
 
   EdgesVector newEdges_;
 
+  /** Adjacent list. */
   GraphMap *graph_;
 
   /** Logger */
