47e2874532424b87ecdfd78034873a91e302833d
Jakub Krajniak <jkrajniak@gmail.com>
2016-04-25 15:23:17 +0200

    update the way how angle is computed

--- src/interaction/TabulatedDihedral.hpp
+++ src/interaction/TabulatedDihedral.hpp
@@ -81,99 +81,97 @@ namespace espressopp {
                         throw std::runtime_error("Tabulated dihedral potential table not available.");
                     }
                 }
-             
+
+                // Kroneker delta function
+                real d(int i, int j) const {
+                  if (i == j)
+                      return 1.0;
+                  else
+                      return 0.0;
+                }
+
+                Real3D prod(Real3D a, Real3D b) const {
+                  Real3D res(0.0, 0.0, 0.0);
+                  for (int i = 0; i < 3; i++)
+                      for (int j = 0; j < 3; j++)
+                          res[i] += (1.0-d(i, j))*a[j]*b[j];
+                  return res;
+                }
+
                 void _computeForceRaw(Real3D& force1,
-                                        Real3D& force2,
-                                        Real3D& force3,
-                                        Real3D& force4,
-                                        const Real3D& dist21,
-                                        const Real3D& dist32,
-                                        const Real3D& dist43) const {
+                                      Real3D& force2,
+                                      Real3D& force3,
+                                      Real3D& force4,
+                                      const Real3D& r21,
+                                      const Real3D& r32,
+                                      const Real3D& r43) const {
                     if (table) {
-                        try {
-                            // compute phi
-                            real dist21_sqr = dist21 * dist21;
-                            real dist32_sqr = dist32 * dist32;
-                            real dist43_sqr = dist43 * dist43;
-                            real dist21_magn = sqrt(dist21_sqr);
-                            real dist32_magn = sqrt(dist32_sqr);
-                            real dist43_magn = sqrt(dist43_sqr);
+                        Real3D retF[4];
 
-                            // cos0
-                            real sb1 = 1.0 / dist21_sqr;
-                            real sb2 = 1.0 / dist32_sqr;
-                            real sb3 = 1.0 / dist43_sqr;
-                            real rb1 = sqrt(sb1);
-                            real rb3 = sqrt(sb3);
-                            real c0 = dist21 * dist43 * rb1 * rb3;
+                        Real3D rijjk = r21.cross(r32);  // [r21 x r32]
+                        Real3D rjkkn = r32.cross(r43);  // [r32 x r43]
 
+                        real rijjk_sqr = rijjk.sqr();
+                        real rjkkn_sqr = rjkkn.sqr();
 
-                            // 1st and 2nd angle
-                            real ctmp = dist21 * dist32;
-                            real r12c1 = 1.0 / (dist21_magn * dist32_magn);
-                            real c1mag = ctmp * r12c1;
+                        real rijjk_abs = sqrt(rijjk_sqr);
+                        real rjkkn_abs = sqrt(rjkkn_sqr);
 
-                            ctmp = (-1.0 * dist32) * dist43;
-                            real r12c2 = 1.0 / (dist32_magn * dist43_magn);
-                            real c2mag = ctmp * r12c2;
+                        // If vectors are collinear then return, it is not
+                        // possible to create a plane and define an angle.
+                        if (rijjk_abs < 0.00000001 || rjkkn_abs < 0.00000001)
+                            return;
 
+                        real inv_rijjk = 1.0 / rijjk_abs;
+                        real inv_rjkkn = 1.0 / rjkkn_abs;
 
-                            //cos and sin of 2 angles and final cos
-                            real sin2 = 1.0 - c1mag * c1mag;
-                            if (sin2 < 0) sin2 = 0.0;
-                            real sc1 = sqrt(sin2);
-                            sc1 = 1.0 / sc1;
+                        // cosine between planes
+                        real cos_phi = (rijjk * rjkkn) * (inv_rijjk * inv_rjkkn);
+                        if (cos_phi > 1.0) cos_phi = 1.0;
+                        else if (cos_phi < -1.0) cos_phi = -1.0;
 
-                            sin2 = 1.0 - c2mag * c2mag;
-                            if (sin2 < 0) sin2 = 0.0;
-                            real sc2 = sqrt(sin2);
-                            sc2 = 1.0 / sc2;
+                        real phi = acos(cos_phi);
+                        real coef1 = table->getForce(phi);
 
-                            real s1 = sc1 * sc1;
-                            real s2 = sc2 * sc2;
-                            real s12 = sc1 * sc2;
-                            real c = (c0 + c1mag * c2mag) * s12;
+                        /** Calculates force in Cartesian coordinates.
+                         * base on: http://www.ccp5.ac.uk/DL_POLY_CLASSIC/MANUALS/USRMAN.pdf
+                         */
+                        real A1 = inv_rijjk * inv_rjkkn;
+                        real A2 = inv_rijjk * inv_rijjk;
+                        real A3 = inv_rjkkn * inv_rjkkn;
 
-                            Real3D cc = dist21.cross(dist32);
-                            real cmag = sqrt(cc * cc);
-                            real dx = cc * dist43 / cmag / dist43_magn;
+                        Real3D p3232(prod(r32, r32));
+                        Real3D p3243(prod(r32, r43));
+                        Real3D p2132(prod(r21, r32));
+                        Real3D p2143(prod(r21, r43));
+                        Real3D p2121(prod(r21, r21));
+                        Real3D p4343(prod(r43, r43));
 
-                            if (c > 1.0) c = 1.0;
-                            else if (c < -1.0) c = -1.0;
+                        // we have 4 particles 1,2,3,4
+                        for (int l = 0; l < 4; l++) {
+                            Real3D B1, B2, B3;
 
-                            // phi
-                            real phi = acos(c);
-                            if (dx < 0.0) phi *= -1.0;
+                            for (int i = 0; i < 3; i++) {
+                                B1[i] = r21[i] * (p3232[i] * (d(l, 2) - d(l, 3)) + p3243[i] * (d(l, 2) - d(l, 1))) +
+                                    r32[i] * (p2132[i] * (d(l, 3) - d(l, 2)) + p3243[i] * (d(l, 1) - d(l, 0))) +
+                                    r43[i] * (p2132[i] * (d(l, 2) - d(l, 1)) + p3232[i] * (d(l, 0) - d(l, 1))) +
+                                    2.0 * r32[i] * p2143[i] * (d(l, 1) - d(l, 2));
 
-                            //phi = 1.0; //testing
+                                B2[i] = 2.0 * r21[i] * (p3232[i] * (d(l, 1) - d(l, 0)) + p2132[i] * (d(l, 1) - d(l, 2))) +
+                                    2.0 * r32[i] * (p2121[i] * (d(l, 2) - d(l, 1)) + p2132[i] * (d(l, 0) - d(l, 1)));
 
-                            // read table
-                            real a = table->getForce(phi);
+                                B3[i] = 2.0 * r43[i] * (p3232[i] * (d(l, 3) - d(l, 2)) + p3243[i] * (d(l, 1) - d(l, 2))) +
+                                    2.0 * r32[i] * (p4343[i] * (d(l, 2) - d(l, 1)) + p3243[i] * (d(l, 2) - d(l, 3)));
+                            }
 
-                            c = c * a;
-                            s12 = s12 * a;
-
-                            real a11 = c * sb1 * s1;
-                            real a22 = -sb2 * (2.0 * c0 * s12 - c * (s1 + s2));
-                            real a33 = c * sb3 * s2;
-                            real a12 = -r12c1 * (c1mag * c * s1 + c2mag * s12);
-                            real a13 = -rb1 * rb3 * s12;
-                            real a23 = r12c2 * (c2mag * c * s2 + c1mag * s12);
-
-                            Real3D sf2 = a12 * dist21 + a22 * dist32 + a23 * dist43;
-
-                            force1 = a11 * dist21 + a12 * dist32 + a13 * dist43;
-                            force2 = (-1.0 * sf2) - force1;
-                            force4 = a13 * dist21 + a23 * dist32 + a33 * dist43;
-                            force3 = sf2 - force4;
-                        } catch (std::exception &e) {
-                            std::cout << "Error in TabulatedDihedral.hpp computeForce, filename="
-                                << filename << std::endl;
-                            std::cout << e.what() << std::endl;
-                            throw e;
+                            retF[l] = coef1 * (A1*B1 - 0.5*cos_phi*(A2*B2 + A3*B3));
                         }
-                    }
-                    else {
+
+                        force1 = retF[0];
+                        force2 = retF[1];
+                        force3 = retF[2];
+                        force4 = retF[3];
+                    } else {
                         throw std::runtime_error("Tabulated dihedral potential table not available.");
                     }
                     
