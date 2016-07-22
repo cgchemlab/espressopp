1fb21931ad4a5e4cd6f50d93970eff2a84106ea0
Jakub Krajniak <jkrajniak@gmail.com>
2016-07-22 19:16:17 +0200

    update documentation

--- src/ParticleRegion.py
+++ src/ParticleRegion.py
@@ -18,29 +18,67 @@
 
 
 r"""
-****************************
+*****************************
 **espressopp.ParticleRegion**
-****************************
+*****************************
+
+Defines a geometrical block and gets the list of particles in that region, updated
+dynamicly during the simulation. There is no limit on number of such regions in the
+simulation box.
+The **ParticleRegion** can store particles of any type or only of the type
+defined byt the **add_type_id**.
+
+The particle is in the region whenever :math:`p > l ^ p < r` where
+:math:`p` is a position of particle, :math:`l`: is the position of left-bottom-front vertex
+of the block and :math:`r` is the position of right-top-rear vertex.
+
+**ParticleRegion** inherits interface from **ParticleGroup**, therefore whenever
+**ParticleGroup** is accepted then **ParticleRegion** can also be passed.
+
+Example of usage
+==================
+
+>>> pr = espressopp.ParticleRegion(storage, espressopp.Real3D(-1, -1, 5), espressopp.Real3D(-1, -1, 11))
 
 
-.. function:: espressopp.ParticleRegion(storage)
+.. function:: espressopp.ParticleRegion(storage, left, right)
 
-		:param storage: 
-		:type storage:
+		:param storage: The storage object.
+		:type storage: espressopp.storage.Storage
+		:param left: The left-bottom-front vertex.
+		:type left: espressopp.Real3D
+		:param right: The right-top-rear vertex.
+		:type right: espressopp.Real3D
 
 .. function:: espressopp.ParticleRegion.has(pid)
+   Check if given particle of *pid* is in the region.
 
 		:param pid: 
 		:type pid: 
 		:rtype: 
 
 .. function:: espressopp.ParticleRegion.show()
-
+   Debug purpose, print all particles in the region.
 		:rtype: 
 
 .. function:: espressopp.ParticleRegion.size()
+   Gets the number of particles in the region.
+		:rtype:
 
-		:rtype: 
+
+.. function:: espressopp.ParticleRegion.add_type_id(type_id)
+   Stores only particles of given type.
+
+        :param type_id: Type id to add
+        :type type_id: int
+
+
+.. function:: espressopp.ParticleRegion.remove_type_id(type_id)
+   Removes type from the list of observed types.
+
+        :param type_id: Type id to remove.
+        :type type_id: int
+
 """
 import _espressopp
 import esutil
